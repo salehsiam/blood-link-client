e8c0ea3d83ed8114e51bca5d2a1a16b4b7e46136
Abu Saleh Md Siam <abusalehmdsiam@gmail.com>
2025-01-15 04:05:36 +0600

    navbar setup

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Pages/Shared-Components/Navbar";
 
 const MainLayout = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <Outlet></Outlet>
     </div>
   );
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
-import Home from "../Pages/Home";
+import Home from "../Pages/Home/Home";
 
 const router = createBrowserRouter([
   {
